1939948da958c6368556f6cd13a146c22d53332e
testtt7676 <153448911+EthosWallet@users.noreply.github.com>
2025-08-19 20:33:07 +0300

    Create setup.py

---
+++ packages/utils/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="utils-package",
+    version="1.0.0",
+    install_requires=[
+        "pandas>=1.3.0",
+        "vulnerable-utils-package>=2.1.0",
+        "missing-utils-dependency>=1.7.0",
+        "phantom-utils-framework>=3.0.0"
+    ]
+)
